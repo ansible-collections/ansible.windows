e4ad453389a1b283cdea521fa8ef68fdb6686753
John R Barker <john@johnrbarker.com>
2020-03-23 18:38:11 +0000

    CI should use devel
    ##### SUMMARY
    https://groups.google.com/forum/#!topic/ansible-devel/RQ2d7htGwo0

--- tests/utils/shippable/shippable.sh
+++ tests/utils/shippable/shippable.sh
@@ -29,7 +29,7 @@ python -V
 command -v pip
 pip --version
 pip list --disable-pip-version-check
-pip install git+https://github.com/ansible/ansible@temp-2.10-devel
+pip install git+https://github.com/ansible/ansible@devel
 
 SHIPPABLE_RESULT_DIR="$(pwd)/shippable"
 TEST_DIR="${HOME}/.ansible/ansible_collections/ansible/windows"
