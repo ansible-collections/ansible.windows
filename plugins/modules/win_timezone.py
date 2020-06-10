296105eb121bdd31427c337d98dbc9e39b1ca852
Jordan Borean <jborean93@gmail.com>
2020-06-10 12:08:35 +1000

    Fix up docs after migration (#95)
    * Fix up docs after migration
    
    * Fix up sanity errors

--- plugins/modules/win_timezone.py
+++ plugins/modules/win_timezone.py
@@ -4,10 +4,6 @@
 # Copyright: (c) 2015, Phil Schwartz <schwartzmx@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-
 DOCUMENTATION = r'''
 ---
 module: win_timezone
@@ -30,26 +26,26 @@ notes:
   U(https://support.microsoft.com/en-us/help/2556308/tzutil-command-line-tool-is-added-to-windows-vista-and-to-windows-server-2008)
   needs to be installed to be able to run this module.
 seealso:
-- module: win_region
+- module: community.windows.win_region
 author:
 - Phil Schwartz (@schwartzmx)
 '''
 
 EXAMPLES = r'''
 - name: Set timezone to 'Romance Standard Time' (GMT+01:00)
-  win_timezone:
+  community.windows.win_timezone:
     timezone: Romance Standard Time
 
 - name: Set timezone to 'GMT Standard Time' (GMT)
-  win_timezone:
+  community.windows.win_timezone:
     timezone: GMT Standard Time
 
 - name: Set timezone to 'Central Standard Time' (GMT-06:00)
-  win_timezone:
+  community.windows.win_timezone:
     timezone: Central Standard Time
 
 - name: Set timezime to Pacific Standard time and disable Daylight Saving time adjustments
-  win_timezone:
+  community.windows.win_timezone:
     timezone: Pacific Standard Time_dstoff
 '''
 
