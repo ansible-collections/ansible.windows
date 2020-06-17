1e386c45c05d6bcf57839ce40c42748703ce1ace
Jordan Borean <jborean93@gmail.com>
2020-06-17 15:41:22 +1000

    Add tests

---
+++ tests/integration/targets/win_dsc/files/xTestClassDsc/1.0.0/xTestClassDsc.psm1
@@ -0,0 +1,34 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSDSCDscExamplesPresent", "")]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSDSCDscTestsPresent", "")]
+param()
+
+enum Ensure
+{
+    Absent
+    Present
+}
+
+[DscResource()]
+class xTestClassDsc
+{
+    [DscProperty(Key)]
+    [Ensure]$Ensure
+
+    [xTestClassDsc] Get()
+    {
+        return $this
+    }
+
+    [bool] Test()
+    {
+        return $true
+    }
+
+    [void] Set()
+    {
+        [System.ServiceProcess.ServiceControllerStatus] $stopped = [System.ServiceProcess.ServiceControllerStatus]::Stopped
+        if (-not $stopped) {
+            throw "test"
+        }
+    }
+}
