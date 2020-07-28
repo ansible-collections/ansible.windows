34019d3a33db4b4b94645a8826aac1a8836b6513
jhawkesworth <jhawkesworth@users.noreply.github.com>
2020-07-28 21:02:20 +0100

    Update email address associated with my contributions to community.windows (#118)

--- plugins/modules/win_regmerge.ps1
+++ plugins/modules/win_regmerge.ps1
@@ -1,6 +1,6 @@
 #!powershell
 
-# Copyright: (c) 2015, Jon Hawkesworth (@jhawkesworth) <figs@unity.demon.co.uk>
+# Copyright: (c) 2015, Jon Hawkesworth (@jhawkesworth) <jhawkesworth@protonmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 #Requires -Module Ansible.ModuleUtils.ArgvParser
--- plugins/modules/win_regmerge.py
+++ plugins/modules/win_regmerge.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2015, Jon Hawkesworth (@jhawkesworth) <figs@unity.demon.co.uk>
+# Copyright: (c) 2015, Jon Hawkesworth (@jhawkesworth) <jhawkesworth@protonmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 DOCUMENTATION = r'''
