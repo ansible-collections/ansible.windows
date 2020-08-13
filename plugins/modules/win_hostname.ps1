cf1ae7698f06dd2d1fb34ec7d4153a21594e744a
Jordan Borean <jborean93@gmail.com>
2020-08-14 07:14:25 +1000

    win_hostname - Basic and diff support (#93)
    * win_hostname - Basic and diff support
    
    * Fix default value

--- plugins/modules/win_hostname.ps1
+++ plugins/modules/win_hostname.ps1
@@ -3,30 +3,32 @@
 # Copyright: (c) 2018, Ripon Banik (@riponbanik)
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-#Requires -Module Ansible.ModuleUtils.Legacy
+#AnsibleRequires -CSharpUtil Ansible.Basic
 
-$ErrorActionPreference = "Stop"
-
-$params = Parse-Args $args -supports_check_mode $true
-$name = Get-AnsibleParam -obj $params -name "name" -type "str" -failifempty $true
-$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+$spec = @{
+    options = @{
+        name = @{ type = 'str'; required = $true }
+    }
+    supports_check_mode = $true
+}
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
 
 $current_computer_name = (Get-CimInstance -Class Win32_ComputerSystem).DNSHostname
 
-$result = @{
-   changed = $false
-   old_name = $current_computer_name
-   reboot_required = $false
-}
+$module.Result.old_name = $current_computer_name
+$module.Result.reboot_required = $false
 
-if ($name -ne $current_computer_name) {
+$module.Diff.before = $current_computer_name
+$module.Diff.after = $module.Params.name
+
+if ($module.Params.name -ne $current_computer_name) {
     Try {
-        Rename-Computer -NewName $name -Force -WhatIf:$check_mode
+        Rename-Computer -NewName $module.Params.name -Force -WhatIf:$module.CheckMode
     } Catch {
-        Fail-Json -obj $result -message "Failed to rename computer to '$name': $($_.Exception.Message)"
+        $module.FailJson("Failed to rename computer to '$($module.Params.name)': $($_.Exception.Message)", $_)
     }
-    $result.changed = $true
-    $result.reboot_required = $true
+    $module.Result.changed = $true
+    $module.Result.reboot_required = $true
 }
 
-Exit-Json -obj $result
+$module.ExitJson()
