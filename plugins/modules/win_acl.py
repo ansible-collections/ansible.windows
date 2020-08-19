a17d6471b27ac077e1021fd192c2afde2841d936
Håkon Lerring <112802+Hakon@users.noreply.github.com>
2020-08-19 09:39:23 +0200

    Use proper parameter- and value linking
    Co-authored-by: Brian Scholer <1260690+briantist@users.noreply.github.com>

--- plugins/modules/win_acl.py
+++ plugins/modules/win_acl.py
@@ -45,7 +45,7 @@ options:
       FileSystemRights U(https://msdn.microsoft.com/en-us/library/system.security.accesscontrol.filesystemrights.aspx).
     - If C(path) is a registry key, rights can be any right under MSDN
       RegistryRights U(https://msdn.microsoft.com/en-us/library/system.security.accesscontrol.registryrights.aspx).
-    - If C(path) is a certificate key, rights can be Read and/or FullControl
+    - If I(path) is a certificate key, rights can be C(Read) and/or C(FullControl).
     type: str
     required: yes
   inherit:
