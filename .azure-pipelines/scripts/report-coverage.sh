b74fc50f424c2374a06b82ce657462ad2b8fbfa2
John Barker <john@johnrbarker.com>
2020-12-11 15:39:00 +0000

    AZP: Install ansible-test for coverage

--- .azure-pipelines/scripts/report-coverage.sh
+++ .azure-pipelines/scripts/report-coverage.sh
@@ -5,4 +5,6 @@ set -o pipefail -eu
 
 PATH="${PWD}/bin:${PATH}"
 
+pip install https://github.com/ansible/ansible/archive/devel.tar.gz --disable-pip-version-check
+
 ansible-test coverage xml --stub --venv --venv-system-site-packages --color -v
