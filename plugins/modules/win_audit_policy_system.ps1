0074f2d161677fb6912e5b3efdd4d968e3691f1d
Jordan Borean <jborean93@gmail.com>
2021-11-11 18:21:45 +1000

    Fix devel sanity checks - ci_complete (#331)

--- plugins/modules/win_audit_policy_system.ps1
+++ plugins/modules/win_audit_policy_system.ps1
@@ -22,23 +22,19 @@ $results = @{
 $categories_rc = run-command -command 'auditpol /list /category /r'
 $subcategories_rc = run-command -command 'auditpol /list /subcategory:* /r'
 
-If ($categories_rc.item('rc') -eq 0)
-{
+If ($categories_rc.item('rc') -eq 0) {
     $categories = ConvertFrom-Csv $categories_rc.item('stdout') | Select-Object -expand Category*
 }
-Else
-{
+Else {
     Fail-Json -obj $results -message "Failed to retrive audit policy categories. Please make sure the auditpol command is functional on
     the system and that the account ansible is running under is able to retrieve them. $($_.Exception.Message)"
 }
 
-If ($subcategories_rc.item('rc') -eq 0)
-{
+If ($subcategories_rc.item('rc') -eq 0) {
     $subcategories = ConvertFrom-Csv $subcategories_rc.item('stdout') | Select-Object -expand Category* |
-    Where-Object {$_ -notin $categories}
+        Where-Object { $_ -notin $categories }
 }
-Else
-{
+Else {
     Fail-Json -obj $results -message "Failed to retrive audit policy subcategories. Please make sure the auditpol command is functional on
     the system and that the account ansible is running under is able to retrieve them. $($_.Exception.Message)"
 }
@@ -55,19 +51,17 @@ $audit_type = Get-AnsibleParam -obj $params -name "audit_type" -type "list" -fai
 ########################
 Function Get-AuditPolicy ($GetString) {
     $auditpolcsv = Run-Command -command $GetString
-    If ($auditpolcsv.item('rc') -eq 0)
-    {
-        $Obj = ConvertFrom-CSV $auditpolcsv.item('stdout') | Select-Object @{n='subcategory';e={$_.Subcategory.ToLower()}},
-        @{n='audit_type';e={$_."Inclusion Setting".ToLower()}}
+    If ($auditpolcsv.item('rc') -eq 0) {
+        $Obj = ConvertFrom-CSV $auditpolcsv.item('stdout') | Select-Object @{n = 'subcategory'; e = { $_.Subcategory.ToLower() } },
+        @{ n = 'audit_type'; e = { $_."Inclusion Setting".ToLower() } }
     }
     Else {
         return $auditpolcsv.item('stderr')
     }
 
     $HT = @{}
-    Foreach ( $Item in $Obj )
-    {
-        $HT.Add($Item.subcategory,$Item.audit_type)
+    Foreach ( $Item in $Obj ) {
+        $HT.Add($Item.subcategory, $Item.audit_type)
     }
     $HT
 }
@@ -77,14 +71,13 @@ Function Get-AuditPolicy ($GetString) {
 ################
 
 #make sure category and subcategory are valid
-If (-Not $category -and -Not $subcategory) {Fail-Json -obj $results -message "You must provide either a Category or Subcategory parameter"}
-If ($category -and $subcategory) {Fail-Json -obj $results -message "Must pick either a specific subcategory or category. You cannot define both"}
+If (-Not $category -and -Not $subcategory) { Fail-Json -obj $results -message "You must provide either a Category or Subcategory parameter" }
+If ($category -and $subcategory) { Fail-Json -obj $results -message "Must pick either a specific subcategory or category. You cannot define both" }
 
 
-$possible_audit_types = 'success','failure','none'
+$possible_audit_types = 'success', 'failure', 'none'
 $audit_type | ForEach-Object {
-    If ($_ -notin $possible_audit_types)
-    {
+    If ($_ -notin $possible_audit_types) {
         Fail-Json -obj $result -message "$_ is not a valid audit_type. Please choose from $($possible_audit_types -join ',')"
     }
 }
@@ -97,17 +90,16 @@ $audit_type_string = $audit_type -join ' and '
 $SetString = 'auditpol /set'
 $GetString = 'auditpol /get /r'
 
-If ($category) {$SetString = "$SetString /category:`"$category`""; $GetString = "$GetString /category:`"$category`""}
-If ($subcategory) {$SetString= "$SetString /subcategory:`"$subcategory`""; $GetString = "$GetString /subcategory:`"$subcategory`""}
+If ($category) { $SetString = "$SetString /category:`"$category`""; $GetString = "$GetString /category:`"$category`"" }
+If ($subcategory) { $SetString = "$SetString /subcategory:`"$subcategory`""; $GetString = "$GetString /subcategory:`"$subcategory`"" }
 
 
-Switch ($audit_type_string)
-{
-    'success and failure' {$SetString = "$SetString /success:enable /failure:enable"; $audit_type_check = $audit_type_string}
-    'failure' {$SetString = "$SetString /success:disable /failure:enable"; $audit_type_check = $audit_type_string}
-    'success' {$SetString = "$SetString /success:enable /failure:disable"; $audit_type_check = $audit_type_string}
-    'none' {$SetString = "$SetString /success:disable /failure:disable"; $audit_type_check = 'No Auditing'}
-    default {Fail-Json -obj $result -message "It seems you have specified an invalid combination of items for audit_type. Please review documentation"}
+Switch ($audit_type_string) {
+    'success and failure' { $SetString = "$SetString /success:enable /failure:enable"; $audit_type_check = $audit_type_string }
+    'failure' { $SetString = "$SetString /success:disable /failure:enable"; $audit_type_check = $audit_type_string }
+    'success' { $SetString = "$SetString /success:enable /failure:disable"; $audit_type_check = $audit_type_string }
+    'none' { $SetString = "$SetString /success:disable /failure:disable"; $audit_type_check = 'No Auditing' }
+    default { Fail-Json -obj $result -message "It seems you have specified an invalid combination of items for audit_type. Please review documentation" }
 }
 
 #########################
@@ -117,8 +109,7 @@ Switch ($audit_type_string)
 $CurrentRule = Get-AuditPolicy $GetString
 
 #exit if the audit_type is already set properly for the category
-If (-not ($CurrentRule.Values | Where-Object {$_ -ne $audit_type_check}) )
-{
+If (-not ($CurrentRule.Values | Where-Object { $_ -ne $audit_type_check }) ) {
     $results.current_audit_policy = Get-AuditPolicy $GetString
     Exit-Json -obj $results
 }
@@ -127,12 +118,10 @@ If (-not ($CurrentRule.Values | Where-Object {$_ -ne $audit_type_check}) )
 ### Apply Change ###
 ####################
 
-If (-not $check_mode)
-{
+If (-not $check_mode) {
     $ApplyPolicy = Run-Command -command $SetString
 
-    If ($ApplyPolicy.Item('rc') -ne 0)
-    {
+    If ($ApplyPolicy.Item('rc') -ne 0) {
         $results.current_audit_policy = Get-AuditPolicy $GetString
         Fail-Json $results "Failed to set audit policy - $($_.Exception.Message)"
     }
