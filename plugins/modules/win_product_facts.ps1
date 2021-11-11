acd6b5ee5825e18792e32dfb9a992ee7069145dc
Jordan Borean <jborean93@gmail.com>
2021-11-11 18:21:45 +1000

    Fix devel sanity checks - ci_complete (#331)

--- plugins/modules/win_product_facts.ps1
+++ plugins/modules/win_product_facts.ps1
@@ -15,7 +15,8 @@ $module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
 # First try to find the product key from ACPI
 try {
     $product_key = (Get-CimInstance -Class SoftwareLicensingService).OA3xOriginalProductKey
-} catch {
+}
+catch {
     $product_key = $null
 }
 
@@ -23,7 +24,8 @@ if (-not $product_key) {
     # Else try to get it from the registry instead
     try {
         $data = Get-ItemPropertyValue -LiteralPath "HKLM:\Software\Microsoft\Windows NT\CurrentVersion" -Name DigitalProductId
-    } catch {
+    }
+    catch {
         $data = $null
     }
 
@@ -31,18 +33,19 @@ if (-not $product_key) {
     if (-not $data) {
         try {
             $data = Get-ItemPropertyValue -LiteralPath "HKLM:\Software\Microsoft\Windows NT\CurrentVersion" -Name DigitalProductId4
-        } catch {
+        }
+        catch {
             $data = $null
         }
     }
 
     if ($data) {
         $product_key = $null
-        $isWin8 = [int]($data[66]/6) -band 1
+        $isWin8 = [int]($data[66] / 6) -band 1
         $HF7 = 0xF7
         $data[66] = ($data[66] -band $HF7) -bOr (($isWin8 -band 2) * 4)
         $hexdata = $data[52..66]
-        $chardata = "B","C","D","F","G","H","J","K","M","P","Q","R","T","V","W","X","Y","2","3","4","6","7","8","9"
+        $chardata = "B", "C", "D", "F", "G", "H", "J", "K", "M", "P", "Q", "R", "T", "V", "W", "X", "Y", "2", "3", "4", "6", "7", "8", "9"
 
         # Decode base24 binary data
         for ($i = 24; $i -ge 0; $i--) {
@@ -56,17 +59,18 @@ if (-not $product_key) {
             $last = $k
         }
 
-        $product_key_tmp1 = $product_key_output.SubString(1,$last)
-        $product_key_tmp2 = $product_key_output.SubString(1,$product_key_output.Length - 1)
+        $product_key_tmp1 = $product_key_output.SubString(1, $last)
+        $product_key_tmp2 = $product_key_output.SubString(1, $product_key_output.Length - 1)
         if ($last -eq 0) {
             $product_key_output = "N" + $product_key_tmp2
-        } else {
+        }
+        else {
             $product_key_output = $product_key_tmp2.Insert($product_key_tmp2.IndexOf($product_key_tmp1) + $product_key_tmp1.Length, "N")
         }
         $num = 0
         $product_key_split = @()
         for ($i = 0; $i -le 4; $i++) {
-            $product_key_split += $product_key_output.SubString($num,5)
+            $product_key_split += $product_key_output.SubString($num, 5)
             $num += 5
         }
         $product_key = $product_key_split -join "-"
