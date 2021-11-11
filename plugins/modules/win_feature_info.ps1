fc2a328ca180815088e100192412f175921c0e85
Jordan Borean <jborean93@gmail.com>
2021-11-11 18:21:45 +1000

    Fix devel sanity checks - ci_complete (#331)

--- plugins/modules/win_feature_info.ps1
+++ plugins/modules/win_feature_info.ps1
@@ -4,8 +4,8 @@
 #AnsibleRequires -CSharpUtil Ansible.Basic
 
 $spec = @{
-    options             = @{
-        name = @{ type = "str"; default = '*'  }
+    options = @{
+        name = @{ type = "str"; default = '*' }
     }
     supports_check_mode = $true
 }
@@ -21,23 +21,23 @@ $features = Get-WindowsFeature -Name $name
 $module.Result.features = @(foreach ($feature in ($features)) {
         # These should closely reflect the options for win_feature
         @{
-            name                           = $feature.Name
-            display_name                   = $feature.DisplayName
-            description                    = $feature.Description
-            installed                      = $feature.Installed
-            install_state                  = $feature.InstallState.ToString()
-            feature_type                   = $feature.FeatureType
-            path                           = $feature.Path
-            depth                          = $feature.Depth
-            depends_on                     = $feature.DependsOn
-            parent                         = $feature.Parent
-            server_component_descriptor    = $feature.ServerComponentDescriptor
-            sub_features                   = $feature.SubFeatures
-            system_service                 = $feature.SystemService
-            best_practices_model_id        = $feature.BestPracticesModelId
-            event_query                    = $feature.EventQuery
-            post_configuration_needed      = $feature.PostConfigurationNeeded
-            additional_info                = $feature.AdditionalInfo
+            name = $feature.Name
+            display_name = $feature.DisplayName
+            description = $feature.Description
+            installed = $feature.Installed
+            install_state = $feature.InstallState.ToString()
+            feature_type = $feature.FeatureType
+            path = $feature.Path
+            depth = $feature.Depth
+            depends_on = $feature.DependsOn
+            parent = $feature.Parent
+            server_component_descriptor = $feature.ServerComponentDescriptor
+            sub_features = $feature.SubFeatures
+            system_service = $feature.SystemService
+            best_practices_model_id = $feature.BestPracticesModelId
+            event_query = $feature.EventQuery
+            post_configuration_needed = $feature.PostConfigurationNeeded
+            additional_info = $feature.AdditionalInfo
         }
         $module.Result.exists = $true
     })
