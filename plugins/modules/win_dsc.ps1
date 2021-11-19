67e0deb6b151e072cb6fe60bd5d8af92df66b134
Mike Kletz <michael.kletz.27@gmail.com>
2021-11-19 15:08:00 -0600

    resource name

--- plugins/modules/win_dsc.ps1
+++ plugins/modules/win_dsc.ps1
@@ -439,7 +439,7 @@ if (-not $resource) {
 # https://github.com/ansible-collections/ansible.windows/issues/15
 if ($resource.ImplementedAs -eq 'Composite') {
     $res = @{
-        msg = "unsupported resource type: composite"
+        msg = "unsupported resource type: $resource_name is a composite"
         failed = $true
     }
     Write-Output -InputObject (ConvertTo-Json -Compress -InputObject $res)
