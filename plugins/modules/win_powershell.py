be7f84c3427b8efa4b02f331bc863d51ff2b4d53
Nico Zanferrari <nicozanf@gmail.com>
2022-12-01 11:41:07 +0100

    Update win_powershell.py

--- plugins/modules/win_powershell.py
+++ plugins/modules/win_powershell.py
@@ -102,8 +102,8 @@ notes:
   based on verbosity. Added in C(1.9.0).
 - Any host/console direct output like C(Write-Host) or C([Console]::WriteLine) is not considered an output object, they are
   returned as a string in I(host_out) and I(host_err).
-- Any host/console pipelined output like C(Write-Output) or C(echo) is also not considered an output object, but they are
-  returned as a list in I(host_output).  
+- Any host/console output stream like C(Write-Output) or C(echo) is also not considered an output object, but they are
+  returned as a list in I(output).  
 - The module will skip running the script when in check mode unless the script defines
   C([CmdletBinding(SupportsShouldProcess)]).
 author:
