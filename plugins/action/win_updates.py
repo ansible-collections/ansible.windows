d27255cbef60458a54ee4c5d9ebe332e8e106291
martin.siddall <martin.siddall@civica.co.uk>
2023-03-11 15:12:13 +0000

    ignore lynt messahe as json module is used ??

--- plugins/action/win_updates.py
+++ plugins/action/win_updates.py
@@ -4,7 +4,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import json
+import json # pylint: disable=unused-import
 import os.path
 import shutil
 import tempfile
