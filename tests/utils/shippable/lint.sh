c3f9b510fb60a8a8ff5de1c61e777a0391375ee9
Jordan Borean <jborean93@gmail.com>
2024-02-06 00:21:03 +0000

    Add ansible-lint sanity test to CI (#581)

---
+++ tests/utils/shippable/lint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o pipefail -eux
+
+# This is aligned with the galaxy-importer used by AH
+# https://github.com/ansible/galaxy-importer/blob/d4b5e6d12088ba452f129f4824bd049be5543358/setup.cfg#L22C4-L22C33
+python -m pip install \
+    'ansible-lint>=6.2.2,<=6.14.3'
+
+ansible-lint
