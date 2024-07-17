34374c750c96c88f8ad346746be64521ac2d04f0
Jordan Borean <jborean93@gmail.com>
2024-07-17 11:21:47 +1000

    win_owner - Better privilege handling
    This change moves the win_owner module to the new Ansible.Basic
    structure that implements better module option handle and testing
    validation. It also attempts to enable special privileges that can be
    used when setting the owner that may not be enabled when using become.

--- plugins/modules/win_owner.ps1
+++ plugins/modules/win_owner.ps1
@@ -3,40 +3,60 @@
 # Copyright: (c) 2015, Hans-Joachim Kliemeck <git@kliemeck.de>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
-#Requires -Module Ansible.ModuleUtils.Legacy
-#Requires -Module Ansible.ModuleUtils.SID
+#AnsibleRequires -CSharpUtil Ansible.Basic
+#AnsibleRequires -CSharpUtil Ansible.Privilege
+#AnsibleRequires -PowerShell ..module_utils._SecurityIdentifier
 
-$result = @{
-    changed = $false
+$spec = @{
+    options = @{
+        path = @{
+            type = 'path'
+            required = $true
+        }
+        recurse = @{
+            type = 'bool'
+            default = $false
+        }
+        user = @{
+            type = 'str'
+            required = $true
+        }
+    }
+    supports_check_mode = $true
 }
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
 
-$params = Parse-Args $args -supports_check_mode $true
-$check_mode = Get-AnsibleParam -obj $params -name "_ansible_check_mode" -type "bool" -default $false
+$path = $module.Params.path
+$recurse = $module.Params.recurse
+$user = $module.Params.user
 
-$path = Get-AnsibleParam -obj $params -name "path" -type "path" -failifempty $true
-$user = Get-AnsibleParam -obj $params -name "user" -type "str" -failifempty $true
-$recurse = Get-AnsibleParam -obj $params -name "recurse" -type "bool" -default $false -resultobj $result
-
-If (-Not (Test-Path -LiteralPath $path)) {
-    Fail-Json $result "$path file or directory does not exist on the host"
+If (-not (Test-Path -LiteralPath $path)) {
+    $module.FailJson("$path file or directory does not exist on the host")
 }
 
 # Test that the user/group is resolvable on the local machine
-$sid = Convert-ToSID -account_name $user
-if (!$sid) {
-    Fail-Json $result "$user is not a valid user or group on the host machine or domain"
+try {
+    $sid = $user | ConvertTo-AnsibleWindowsSecurityIdentifier -ErrorAction Stop
+}
+catch {
+    $module.FailJson([string]$_, $_)
 }
 
+$privEnabler = New-Object -TypeName Ansible.Privilege.PrivilegeEnabler -ArgumentList @(
+    $false
+    'SeBackupPrivilege' # Opens files without explicit rights
+    'SeChangeNotifyPrivilege' # Bypass parent folder access checks
+    'SeRestorePrivilege' # Sets DACL rules without explicit rights
+    'SeTakeOwnershipPrivilege'  # Sets owner without explicit rights
+)
 Try {
-    $objUser = New-Object System.Security.Principal.SecurityIdentifier($sid)
-
     $file = Get-Item -LiteralPath $path
     $acl = Get-Acl -LiteralPath $file.FullName
 
-    If ($acl.getOwner([System.Security.Principal.SecurityIdentifier]) -ne $objUser) {
-        $acl.setOwner($objUser)
-        Set-Acl -LiteralPath $file.FullName -AclObject $acl -WhatIf:$check_mode
-        $result.changed = $true
+    If ($acl.GetOwner([System.Security.Principal.SecurityIdentifier]) -ne $sid) {
+        $acl.SetOwner($sid)
+        Set-Acl -LiteralPath $file.FullName -AclObject $acl -WhatIf:$module.CheckMode
+        $module.Result.changed = $true
     }
 
     If ($recurse -and $file -is [System.IO.DirectoryInfo]) {
@@ -45,16 +65,19 @@ Try {
         ForEach ($file in $files) {
             $acl = Get-Acl -LiteralPath $file.FullName
 
-            If ($acl.getOwner([System.Security.Principal.SecurityIdentifier]) -ne $objUser) {
-                $acl.setOwner($objUser)
-                Set-Acl -LiteralPath $file.FullName -AclObject $acl -WhatIf:$check_mode
-                $result.changed = $true
+            If ($acl.GetOwner([System.Security.Principal.SecurityIdentifier]) -ne $sid) {
+                $acl.SetOwner($sid)
+                Set-Acl -LiteralPath $file.FullName -AclObject $acl -WhatIf:$module.CheckMode
+                $module.Result.changed = $true
             }
         }
     }
 }
 Catch {
-    Fail-Json $result "an error occurred when attempting to change owner on $path for $($user): $($_.Exception.Message)"
+    $module.FailJson("an error occurred when attempting to change owner on $path for $($user): $($_.Exception.Message)", $_)
+}
+Finally {
+    $privEnabler.Dispose()
 }
 
-Exit-Json $result
+$module.ExitJson()
