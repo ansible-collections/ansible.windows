8ff19c69942a5d577ac8a460a8e8b63d2d3386be
Jordan Borean <jborean93@gmail.com>
2024-08-29 07:09:32 +1000

    Expand connection plugins used in CI (#651)
    Expands the testing matrix of the Windows connection plugins used in CI
    to cover all the supported connections of Windows.

--- tests/utils/shippable/windows.sh
+++ tests/utils/shippable/windows.sh
@@ -6,9 +6,11 @@ declare -a args
 IFS='/:' read -ra args <<< "$1"
 
 version="${args[1]}"
+connection="${args[2]}"
+connection_setting="${args[3]}"
 
-if [ "${#args[0]}" -gt 2 ]; then
-    target="shippable/windows/group${args[2]}/"
+if [ "${#args[0]}" -gt 4 ]; then
+    target="shippable/windows/group${args[4]}/"
 else
     target="shippable/windows/"
 fi
@@ -18,4 +20,7 @@ provider="${P:-default}"
 
 # shellcheck disable=SC2086
 ansible-test windows-integration --color -v --retry-on-error "${target}" ${COVERAGE:+"$COVERAGE"} ${CHANGED:+"$CHANGED"} ${UNSTABLE:+"$UNSTABLE"} \
-    --windows "${version}" --docker default --remote-terminate always --remote-stage "${stage}" --remote-provider "${provider}"
+    --controller "docker:default" \
+    --target "remote:windows/${version},connection=${connection}+${connection_setting},provider=${provider}" \
+    --remote-terminate always --remote-stage "${stage}"
+
