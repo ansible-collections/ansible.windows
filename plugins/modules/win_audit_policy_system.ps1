9f7bb44c4dd4e65927829f550c907fcae4640e28
Amit Weinstock <aweinsto@redhat.com>
2025-01-08 15:42:55 +0200

    migrate module to basic structure

--- plugins/modules/win_audit_policy_system.ps1
+++ plugins/modules/win_audit_policy_system.ps1
@@ -2,36 +2,15 @@
 
 # Copyright: (c) 2017, Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 #AnsibleRequires -CSharpUtil Ansible.Basic
-#Requires -Module Ansible.ModuleUtils.CommandUtil
-
-
-
-$categories_rc = run-command -command 'auditpol /list /category /r'
-$subcategories_rc = run-command -command 'auditpol /list /subcategory:* /r'
-
-If ($categories_rc.item('rc') -eq 0) {
-    $categories = ConvertFrom-Csv $categories_rc.item('stdout') | Select-Object -expand Category*
-}
-Else {
-    $module.FailJson("Failed to retrive audit policy categories. Please make sure the auditpol command is functional on
-    the system and that the account ansible is running under is able to retrieve them." , $($_.Exception.Message))
-}
-
-If ($subcategories_rc.item('rc') -eq 0) {
-    $subcategories = ConvertFrom-Csv $subcategories_rc.item('stdout') | Select-Object -expand Category* |
-        Where-Object { $_ -notin $categories }
-}
-Else {
-    $module.FailJson("Failed to retrive audit policy subcategories. Please make sure the auditpol command is functional on
-    the system and that the account ansible is running under is able to retrieve them." , $($_.Exception.Message))
-}
-
+##Requires -Module Ansible.ModuleUtils.AddType
+#AnsibleRequires -PowerShell ..module_utils.Process
 $spec = @{
     options = @{
-        category = @{ type = 'str' ; choices = $categories }
-        subcategory = @{ type = 'str' ; choices = $subcategories }
-        audit_type = @{ type = 'list'; elements = 'str' ; required = $true }
+        category = @{ type = 'str' }
+        subcategory = @{ type = 'str' }
+        audit_type = @{ type = 'list'; elements = 'str' ; required = $true ; choices = @("failure", "none", "success") }
     }
     supports_check_mode = $true
     mutually_exclusive = @(
@@ -41,52 +20,94 @@ $spec = @{
         , @('subcategory', 'category')
     )
 }
-$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
 
-$category = $module.Params.category
-$subcategory = $module.Params.subcategory
-$audit_type = $module.Params.audit_type
-$check_mode = $module.Checkmode
-Function Get-AuditPolicy ($GetString) {
-    $auditpolcsv = Run-Command -command $GetString
-    If ($auditpolcsv.item('rc') -eq 0) {
-        $Obj = ConvertFrom-CSV $auditpolcsv.item('stdout') | Select-Object @{n = 'subcategory'; e = { $_.Subcategory.ToLower() } },
-        @{ n = 'audit_type'; e = { $_."Inclusion Setting".ToLower() } }
+
+Function Get-AuditPolicy {
+    param (
+        [string]$Command
+    )
+    $categoriesParams = @{
+        CommandLine = $Command
+    }
+    $auditpolcsv = Start-AnsibleWindowsProcess @categoriesParams
+    If ($($auditpolcsv.ExitCode) -eq 0) {
+        $Obj = ConvertFrom-CSV $($auditpolcsv.Stdout) | Select-Object @{
+            n = 'subcategory'
+            e = { $_.Subcategory.ToLower() }
+        },
+        @{
+            n = 'audit_type'
+            e = { $_."Inclusion Setting".ToLower() }
+        }
     }
     Else {
-        return $auditpolcsv.item('stderr')
+        return $($auditpolcsv.Stderr)
     }
 
     $HT = @{}
-    Foreach ( $Item in $Obj ) {
-        $HT.Add($Item.subcategory, $Item.audit_type)
+    Foreach ($Item in $Obj) {
+        $HT.Add($($Item.subcategory), $($Item.audit_type))
     }
     $HT
 }
 
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+$category = $module.Params.category
+$subCategory = $module.Params.subcategory
+$audit_type = $module.Params.audit_type
+$check_mode = $module.Checkmode
 
+$categoriesParams = @{ CommandLine = "auditpol /list /category /r" }
+$categories_rc = Start-AnsibleWindowsProcess @categoriesParams
+$subcategoriesParams = @{ CommandLine = "auditpol /list /subcategory:* /r" }
+$subcategories_rc = Start-AnsibleWindowsProcess @subcategoriesParams
 
-$SetString = 'auditpol /set'
-$GetString = 'auditpol /get /r'
-
-If ($category) { $SetString = "$SetString /category:`"$category`""; $GetString = "$GetString /category:`"$category`"" }
-Elseif ($subcategory) { $SetString = "$SetString /subcategory:`"$subcategory`""; $GetString = "$GetString /subcategory:`"$subcategory`"" }
-
-if ('success' -in $audit_type -and 'failure' -in $audit_type) {
-    $SetString = "$SetString /success:enable /failure:enable"; $audit_type_check = "success and failure"
-}
-Elseif ( 'success' -in $audit_type ) {
-    $SetString = "$SetString /success:enable /failure:disable"; $audit_type_check = "success"
-}
-Elseif ( 'failure' -in $audit_type ) {
-    $SetString = "$SetString /success:disable /failure:enable"; $audit_type_check = "failure"
+If ($categories_rc.ExitCode -eq 0) {
+    $categories = ConvertFrom-Csv $categories_rc.Stdout | Select-Object -expand Category*
 }
 Else {
-    $SetString = "$SetString /success:disable /failure:disable"; $audit_type_check = 'No Auditing'
+    $module.FailJson("Failed to retrive audit policy categories: $categories_rc.Stderr")
+}
+
+If ($subcategories_rc.ExitCode -eq 0) {
+    $subcategories = ConvertFrom-Csv $subcategories_rc.Stdout | Select-Object -ExpandProperty 'Category*' | Where-Object { $_ -notin $categories }
+}
+Else {
+    $module.FailJson("Failed to retrieve audit policy subcategories: $subcategories_rc.Stderr")
+}
+# Validate user inputs. The avaible choices is known in run time after the module utils import.
+If ($category -and $category -notin $categories) { $module.FailJson("Invalid category provided") }
+If ($subcategory -and $subcategory -notin $subcategories) { $module.FailJson("Invalid sub category provided") }
+
+
+$SetCommand = 'auditpol /set'
+$GetCommand = 'auditpol /get /r'
+
+If ($category) {
+    $SetCommand = "$SetCommand /category:`"$category`""
+    $GetCommand = "$GetCommand /category:`"$category`""
+}
+Else {
+    $SetCommand = "$SetCommand /subcategory:`"$subcategory`""
+    $GetCommand = "$GetCommand /subcategory:`"$subcategory`""
+}
+
+if ('success' -in $audit_type -and 'failure' -in $audit_type) {
+    $SetCommand = "$SetCommand /success:enable /failure:enable"; $audit_type_check = "success and failure"
+}
+Elseif ( 'success' -in $audit_type ) {
+    $SetCommand = "$SetCommand /success:enable /failure:disable"; $audit_type_check = "success"
+}
+Elseif ( 'failure' -in $audit_type ) {
+    $SetCommand = "$SetCommand /success:disable /failure:enable"; $audit_type_check = "failure"
+}
+Else {
+    $SetCommand = "$SetCommand /success:disable /failure:disable"; $audit_type_check = 'No Auditing'
 }
 
 
-$CurrentRule = Get-AuditPolicy $GetString
+$CurrentRule = Get-AuditPolicy $GetCommand
+
 #exit if the audit_type is already set properly for the category
 If (-not ($CurrentRule.Values | Where-Object { $_ -ne $audit_type_check }) ) {
     $module.result.current_audit_policy = $CurrentRule
@@ -94,14 +115,17 @@ If (-not ($CurrentRule.Values | Where-Object { $_ -ne $audit_type_check }) ) {
 }
 
 If (-not $check_mode) {
-    $ApplyPolicy = Run-Command -command $SetString
+    $categoriesParams = @{
+        CommandLine = $SetCommand
+    }
+    $ApplyPolicy = Start-AnsibleWindowsProcess @categoriesParams
 
-    If ($ApplyPolicy.Item('rc') -ne 0) {
-        $module.result.current_audit_policy = Get-AuditPolicy $GetString
-        $module.FailJson("Failed to set audit policy $($_.Exception.Message)")
+    If ($ApplyPolicy.ExitCode -ne 0) {
+        $module.result.current_audit_policy = Get-AuditPolicy $GetCommand
+        $module.FailJson("Failed to set audit policy $($ApplyPolicy.Stderr)")
     }
 }
 
 $module.result.changed = $true
-$module.result.current_audit_policy = Get-AuditPolicy $GetString
+$module.result.current_audit_policy = Get-AuditPolicy $GetCommand
 $module.ExitJson()
