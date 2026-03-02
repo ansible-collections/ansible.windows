5223c6ae175b13ca5b72ec922c40dd0b6ba20f76
Jordan Borean <jborean93@gmail.com>
2026-03-03 07:13:49 +1000

    Prepare for v3.4.0 release (#872)

--- .azure-pipelines/commands/lint.sh
+++ .azure-pipelines/commands/lint.sh
@@ -3,11 +3,10 @@
 set -o pipefail -eux
 
 # This is aligned with the galaxy-importer used by AH.
-# Check what galaxy_importer_version is used in the galaxy project at
-# https://galaxy.ansible.com/api/
-
-# Then check the ansible-lint upper bound at
-# https://github.com/ansible/galaxy-importer/blob/v${VERSION}/setup.cfg
+# Use this script to get the galaxy-importer setup.cfg URL.
+# VERSION=$( curl -s https://galaxy.ansible.com/api/ | jq -r '.galaxy_importer_version' )
+# echo "https://github.com/ansible/galaxy-importer/blob/v${VERSION}/setup.cfg"
+# Then check the ansible-lint upper bound to specify here.
 
 python -m pip install \
     'ansible-lint==25.5.0'
