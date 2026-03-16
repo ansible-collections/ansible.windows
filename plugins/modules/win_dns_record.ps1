e2f07699b2ab06111c869e1861d2318e0e53d575
Hen Yaish <55248011+Yaish25491@users.noreply.github.com>
2026-03-16 11:28:31 +0200

    Remediate Insecure Default DNS Record Permissions (#873)
    This commit removes the hardcoded -AllowUpdateAny flag for non-SRV records in win_dns_record and introduces the allow_update_any parameter (defaulting to false) to secure DNS records by default.
    
    Fixes ACA-5193

--- plugins/modules/win_dns_record.ps1
+++ plugins/modules/win_dns_record.ps1
@@ -13,6 +13,7 @@ $spec = @{
         state = @{ type = "str"; choices = "absent", "present"; default = "present" }
         ttl = @{ type = "int"; default = "3600" }
         aging = @{ type = "bool"; default = $false }
+        allow_update_any = @{ type = "bool"; default = $false }
         type = @{ type = "str"; choices = "A", "AAAA", "CNAME", "DHCID", "NS", "PTR", "SRV", "TXT"; required = $true }
         value = @{ type = "list"; elements = "str"; default = @() ; aliases = @( 'values' ) }
         weight = @{ type = "int" }
@@ -31,6 +32,7 @@ $priority = $module.Params.priority
 $state = $module.Params.state
 $ttl = $module.Params.ttl
 $aging = $module.Params.aging
+$allow_update_any = $module.Params.allow_update_any
 $type = $module.Params.type
 $values = $module.Params.value
 $weight = $module.Params.weight
@@ -49,6 +51,9 @@ if ($null -ne $zone_scope) {
 }
 if ($aging -eq $true) {
     $extra_args_new_records.AgeRecord = $true
+}
+if ($allow_update_any -eq $true) {
+    $extra_args_new_records.AllowUpdateAny = $true
 }
 
 if ($state -eq 'present') {
@@ -187,7 +192,7 @@ if ($null -ne $values -and $values.Count -gt 0) {
                 Add-DnsServerResourceRecord -SRV -Name $name -ZoneName $zone @srv_args @extra_args @extra_args_new_records -WhatIf:$module.CheckMode
             }
             else {
-                Add-DnsServerResourceRecord -Name $name -AllowUpdateAny -ZoneName $zone -TimeToLive $ttl @splat_args -WhatIf:$module.CheckMode `
+                Add-DnsServerResourceRecord -Name $name -ZoneName $zone -TimeToLive $ttl @splat_args -WhatIf:$module.CheckMode `
                     @extra_args @extra_args_new_records
             }
         }
