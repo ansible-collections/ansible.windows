7acad5c14955d23cce53ed41e6cea5d1da513ad3
Mike Morency <mikemorency93@gmail.com>
2026-08-06 11:35:15 -0400

    add win_reboot_info module

---
+++ plugins/modules/win_reboot_info.ps1
@@ -0,0 +1,183 @@
+#!powershell
+
+# Copyright: (c) 2026, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+
+function Get-LastBootTime {
+    param([Ansible.Basic.AnsibleModule]$Module)
+
+    try {
+        $epochStart = New-Object -TypeName DateTime -ArgumentList @(1970, 1, 1, 0, 0, 0, [DateTimeKind]::Utc)
+        $lastBoot = (Get-CimInstance -ClassName Win32_OperatingSystem -Property LastBootUpTime).LastBootUpTime
+        $utcBoot = $lastBoot.ToUniversalTime()
+
+        return @{
+            last_boot_time = $utcBoot.ToString("yyyy-MM-ddTHH:mm:ssZ")
+            last_boot_time_epoch = (New-TimeSpan -Start $epochStart -End $utcBoot).TotalSeconds
+            utc_datetime = $utcBoot
+        }
+    }
+    catch {
+        $Module.FailJson("Failed to get last boot time from CIM: $($_.Exception.Message)")
+    }
+}
+
+function Get-LastRebootEvent {
+    param(
+        [Ansible.Basic.AnsibleModule]$Module,
+        [DateTime]$BootTime
+    )
+
+    try {
+        $rebootEvent = Get-WinEvent -FilterHashtable @{
+            LogName = 'System'
+            Id = 1074
+        } -MaxEvents 1 -ErrorAction SilentlyContinue
+
+        if ($rebootEvent) {
+            $eventTime = $rebootEvent.TimeCreated.ToUniversalTime()
+            if ($eventTime -lt $BootTime.AddMinutes(-5)) {
+                return $null
+            }
+
+            return @{
+                process = $rebootEvent.Properties[0].Value
+                reason = $rebootEvent.Properties[2].Value
+                type = $rebootEvent.Properties[4].Value
+                comment = $rebootEvent.Properties[5].Value
+                initiated_by = $rebootEvent.Properties[6].Value
+                event_time = $eventTime.ToString("yyyy-MM-ddTHH:mm:ssZ")
+            }
+        }
+    }
+    catch {
+        $Module.Warn("Failed to query the System event log for reboot events: $($_.Exception.Message)")
+    }
+
+    return $null
+}
+
+function Test-ComponentBasedServicing {
+    $cbsPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending'
+    if (Test-Path -LiteralPath $cbsPath) {
+        return @{
+            source = 'component_based_servicing'
+            description = 'Component Based Servicing has a pending reboot.'
+        }
+    }
+}
+
+function Test-WindowsUpdate {
+    $wuPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired'
+    if (Test-Path -LiteralPath $wuPath) {
+        return @{
+            source = 'windows_update'
+            description = 'Windows Update has installed updates that require a reboot.'
+        }
+    }
+}
+
+function Test-PendingFileRename {
+    $params = @{
+        LiteralPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager'
+        Name = 'PendingFileRenameOperations'
+        ErrorAction = 'SilentlyContinue'
+    }
+    $pendingRenames = (Get-ItemProperty @params)."$($params.Name)"
+    if ($pendingRenames) {
+        return @{
+            source = 'pending_file_rename'
+            description = 'There are pending file rename operations that require a reboot.'
+        }
+    }
+}
+
+function Test-PendingComputerRename {
+    $pendingParams = @{
+        LiteralPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName'
+        Name = 'ComputerName'
+        ErrorAction = 'SilentlyContinue'
+    }
+    $activeParams = @{
+        LiteralPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName'
+        Name = 'ComputerName'
+        ErrorAction = 'SilentlyContinue'
+    }
+    $pendingName = (Get-ItemProperty @pendingParams)."$($pendingParams.Name)"
+    $activeName = (Get-ItemProperty @activeParams)."$($activeParams.Name)"
+    if ($pendingName -and $activeName -and $pendingName -ne $activeName) {
+        return @{
+            source = 'pending_computer_rename'
+            description = "Computer name change pending from '$activeName' to '$pendingName'."
+        }
+    }
+}
+
+function Test-DomainJoin {
+    $params = @{
+        LiteralPath = 'HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon'
+        ErrorAction = 'SilentlyContinue'
+    }
+    $props = Get-ItemProperty @params
+    if ($props -and ($props.PSObject.Properties.Name -contains 'JoinDomain' -or $props.PSObject.Properties.Name -contains 'AvoidSpnSet')) {
+        return @{
+            source = 'domain_join'
+            description = 'A domain join operation is pending and requires a reboot.'
+        }
+    }
+}
+
+function Test-ServerManager {
+    $params = @{
+        LiteralPath = 'HKLM:\SOFTWARE\Microsoft\ServerManager\ServicingStorage\ServerComponentCache'
+        Name = 'RestartRequired'
+        ErrorAction = 'SilentlyContinue'
+    }
+    $restart = (Get-ItemProperty @params)."$($params.Name)"
+    if ($restart) {
+        return @{
+            source = 'server_manager'
+            description = 'Server Manager indicates a component change requires a reboot.'
+        }
+    }
+}
+
+function Get-PendingRebootReason {
+    <#
+    .SYNOPSIS
+    Checks multiple registry sources for pending reboot indicators and returns a list of matching reasons.
+    If a check is null, that result is filtered out.
+    #>
+    $checks = @(
+        Test-ComponentBasedServicing
+        Test-WindowsUpdate
+        Test-PendingFileRename
+        Test-PendingComputerRename
+        Test-DomainJoin
+        Test-ServerManager
+    ) | Where-Object { $_ }
+
+    return , @($checks)
+}
+
+$spec = @{
+    options = @{}
+    supports_check_mode = $true
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+
+$bootTime = Get-LastBootTime -Module $module
+$module.Result.last_reboot = @{
+    time = $bootTime.last_boot_time
+    time_epoch = $bootTime.last_boot_time_epoch
+    details = Get-LastRebootEvent -Module $module -BootTime $bootTime.utc_datetime
+}
+
+$reasons = Get-PendingRebootReason
+$module.Result.pending_reboot = $reasons.Count -gt 0
+$module.Result.pending_reboot_reasons = @($reasons)
+
+$module.ExitJson()
---
+++ plugins/modules/win_reboot_info.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2026, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+DOCUMENTATION = r'''
+---
+module: win_reboot_info
+version_added: 3.8.0
+short_description: Get reboot status information for a Windows host
+description:
+- Returns information about the last boot time and whether a reboot is pending on the target Windows host.
+- A pending reboot can be caused by Windows Update, Component Based Servicing, pending file rename operations,
+  domain join operations, or Server Manager component changes.
+options: {}
+seealso:
+- module: ansible.windows.win_reboot
+author:
+- Mike Morency (@mikemorency)
+'''
+
+EXAMPLES = r'''
+- name: Get reboot info
+  ansible.windows.win_reboot_info:
+  register: reboot_info
+
+- name: Reboot if required
+  ansible.windows.win_reboot:
+  when: reboot_info.pending_reboot
+
+- name: Display last boot time
+  ansible.builtin.debug:
+    msg: "Last boot: {{ reboot_info.last_reboot.time }}"
+
+- name: Show who initiated the last reboot
+  ansible.builtin.debug:
+    msg: >-
+      Last reboot was initiated by {{ reboot_info.last_reboot.details.initiated_by }}
+      via {{ reboot_info.last_reboot.details.process }}
+      with comment: {{ reboot_info.last_reboot.details.comment }}
+  when: reboot_info.last_reboot.details is not none
+
+- name: Show pending reboot sources
+  ansible.builtin.debug:
+    msg: "Pending reboot due to: {{ reboot_info.pending_reboot_reasons | map(attribute='source') | list }}"
+  when: reboot_info.pending_reboot
+'''
+
+RETURN = r'''
+last_reboot:
+  description: Information about the last system boot.
+  returned: always
+  type: dict
+  contains:
+    time:
+      description: The last boot time of the system as an ISO 8601 UTC timestamp.
+      type: str
+      sample: "2024-01-15T08:30:00Z"
+    time_epoch:
+      description: The last boot time as a Unix epoch timestamp in seconds.
+      type: float
+      sample: 1705305000.0
+    details:
+      description:
+      - Details about the most recent shutdown or restart event from the Windows Event Log.
+      - This is gathered from Event ID 1074 in the System log.
+      - Will be C(null) if no shutdown event was found in the event log.
+      type: dict
+      contains:
+        initiated_by:
+          description: The user account that initiated the reboot.
+          type: str
+          sample: "DOMAIN\\admin"
+        process:
+          description: The process that triggered the reboot.
+          type: str
+          sample: "C:\\Windows\\system32\\shutdown.exe"
+        reason:
+          description: The reason category for the reboot.
+          type: str
+          sample: "Operating System: Recovery (Planned)"
+        comment:
+          description: The comment or message provided when the reboot was initiated.
+          type: str
+          sample: "Reboot initiated by Ansible"
+        type:
+          description: The type of action, such as C(restart), C(shutdown), or C(power off).
+          type: str
+          sample: "restart"
+        event_time:
+          description: The time the shutdown event was logged as an ISO 8601 UTC timestamp.
+          type: str
+          sample: "2024-01-15T08:29:45Z"
+pending_reboot:
+  description: Whether the system has a pending reboot from any source.
+  returned: always
+  type: bool
+  sample: true
+pending_reboot_reasons:
+  description:
+  - A list of sources that require a reboot.
+  - Will be an empty list if no reboot is pending.
+  returned: always
+  type: list
+  elements: dict
+  contains:
+    source:
+      description:
+      - The source that requires a reboot.
+      - Known sources are C(component_based_servicing), C(windows_update), C(pending_file_rename),
+        C(pending_computer_rename), C(domain_join), C(server_manager).
+      type: str
+      sample: windows_update
+    description:
+      description: A human-readable description of why a reboot is pending.
+      type: str
+      sample: Windows Update has installed updates that require a reboot.
+'''
